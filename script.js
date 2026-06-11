624bbddf6f429213f37dcdaf55622b3e1fbee90f
Faraz Zakhwan <awanatyas@gmail.com>
2026-06-11 12:42:30 +0700

    Tambah Fitur Search

--- script.js
+++ script.js
@@ -40,3 +40,52 @@ function saveNote(){
 
     alert("Catatan berhasil disimpan!");
 }
+
+function searchNotes(){
+
+    let keyword =
+        document
+        .getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    let resultContainer =
+        document
+        .getElementById("searchResults");
+
+    resultContainer.innerHTML = "";
+
+    if(keyword === ""){
+        return;
+    }
+
+    for(let i = 0; i < localStorage.length; i++){
+
+        let key = localStorage.key(i);
+
+        let note =
+            localStorage.getItem(key);
+
+        if(
+            key.toLowerCase().includes(keyword)
+            ||
+            note.toLowerCase().includes(keyword)
+        ){
+
+            resultContainer.innerHTML += `
+                <div
+                    class="result-card"
+                    onclick="openNote('${key}')"
+                >
+
+                    <h3>${key}</h3>
+
+                    <p>
+                    ${note.substring(0,120)}...
+                    </p>
+
+                </div>
+            `;
+        }
+    }
+}
